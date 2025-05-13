b8974699a8be1fe2f5d060629f337ba34745913b
aleGuerriero <alessandro.guerriero@live.it>
2025-05-13 15:54:27 +0200

    right preproc

--- src/Preprocessing/DataPreprocessing.py
+++ src/Preprocessing/DataPreprocessing.py
@@ -7,26 +7,34 @@ from transformers import RobertaTokenizer, RobertaModel
 
 import torch
 
-root = "data/IEMOCAP/"
-sessions = [f"Session{i}" for i in range(1, 3)]
+root = "./data/IEMOCAP/"
+sessions = [f"Session{i}" for i in range(1, 6)]
 
+
+videoTextConv = {}
+videoAudioConv = {}
+videoSpeakersConv = {}
+videoLabelsConv = {}
+videoTimeConv = {}
 
 videoText = {}
 videoAudio = {}
 videoSpeakers = {}
 videoLabels = {}
+
+
 trainVid, testVid = [], []
 
 # happy, sad, angry, neutral, frustrated, excited, surprised, fearful, disgusted, indefinite, other
-label_map = {'hap': 0, 'sad': 1, 'ang': 2, 'neu': 3, 'fru':4, 'exc':5,
-             'sur':6, 'fea':7, 'dis':8, 'xxx':9, 'oth':10}
+label_map = {'hap': 0, 'sad': 1, 'ang': 2, 'neu': 3, 'fru':4, 'exc':0,
+             'sur':5, 'fea':5, 'dis':5,'xxx':5, 'oth':5}
 
 tokenizer = RobertaTokenizer.from_pretrained("roberta-base")
 model = RobertaModel.from_pretrained("roberta-base", add_pooling_layer=False)
 model.eval()
 
 smile = opensmile.Smile(
-    feature_set=opensmile.FeatureSet.eGeMAPSv01b,
+    feature_set=opensmile.FeatureSet.eGeMAPSv02,
     feature_level=opensmile.FeatureLevel.Functionals
 )
 
@@ -49,6 +57,7 @@ for session in sessions:
                     print(f"{idx}")
                     parts = textline.strip().split()
                     vid = parts[3]
+                    print(f'VID: {vid}')
                     emotion = parts[4]
                     if emotion not in label_map:
                         #continue
@@ -71,47 +80,72 @@ for session in sessions:
 
                     with torch.no_grad():
                         outputs = model(**inputs).last_hidden_state.mean(dim=1).squeeze(0)
-                    videoText.setdefault(vid, []).append(outputs.numpy())
+                    videoTextConv.setdefault(vid, []).append(outputs.numpy())
 
 
                     # 2. AUDIO
                     wav = os.listdir(wav_fold_path)[idx]
+                    print(f'WAV: {wav}')
                     wav_path = wav_dir + "/" + wav_fold + "/" + wav
                     idx = idx + 1
 
                     try:
                         features = smile.process_file(wav_path)  # pandas DataFrame
                         audio_feat = features.iloc[0].to_numpy()  # numpy vector
-                        videoAudio.setdefault(vid, []).append(audio_feat)
+                        videoAudioConv.setdefault(vid, []).append(audio_feat)
                     except Exception as e:
                         print(f"Errore nell'estrazione audio per {vid}: {e}")
 
 
                     # 3. Speaker
-                    speaker = 'M' if vid[7] == 'M' else 'F'
-                    videoSpeakers.setdefault(vid, []).append(speaker)
+                    speaker = [1,0] if vid.split("_")[-1][0] == 'M' else [0,1]
+                    videoSpeakersConv.setdefault(vid, []).append(speaker)
 
 
                     # 4. Label
-                    videoLabels.setdefault(vid, []).append(label_map[emotion])
+                    videoLabelsConv.setdefault(vid, []).append(label_map[emotion])
+
+                    # 5. Time
+                    time = parts[0][1:]
+                    time = float(time)
+                    videoTimeConv.setdefault(vid, []).append(time)
 
 
-                    # 5. Train/Test division
-                    if session == "Session1":
-                        trainVid.append(vid)
-                    else:
-                        testVid.append(vid)
+            sortedKey = sorted(videoTimeConv, key=videoTimeConv.get)
 
-print("Numero video in train:", len(trainVid))
-print("Numero video in test:", len(testVid))
-print("Esempi:", trainVid[:5])
+            videoTextConv = {k: videoTextConv[k] for k in sortedKey}
+            videoAudioConv = {k: videoAudioConv[k] for k in sortedKey}
+            videoSpeakersConv = {k: videoSpeakersConv[k] for k in sortedKey}
+            videoLabelsConv = {k: videoLabelsConv[k] for k in sortedKey}
+
+
+            convID = emo_file.split(".")[0]
+            videoText.setdefault(convID, []).extend(videoTextConv.values())
+            videoAudio.setdefault(convID, []).extend(videoAudioConv.values())
+            videoSpeakers.setdefault(convID, []).extend(videoSpeakersConv.values()) #[['F'] , ['M']]
+            videoLabels.setdefault(convID, []).extend(videoLabelsConv.values())
+
+            videoTextConv = {}
+            videoAudioConv = {}
+            videoSpeakersConv = {}
+            videoLabelsConv = {}
+            videoTimeConv = {}
+
+
+        # 5. Train/Test division
+        if session != "Session5":
+            trainVid.append(convID)
+        else:
+            testVid.append(convID)
+
+print("#Conversation in train:", len(trainVid))
+print("#Conversation in Test:", len(testVid))
+
 
 # Save file pickle
-with open("iemocap_multimodal_features.pkl", "wb") as f:
+with open("../../data/iemocap_multimodal_features_6labels.pkl", "wb") as f:
     pickle.dump(
         (list(videoText.keys()), videoSpeakers, videoLabels, videoText,
          None, None, None, videoAudio, trainVid, testVid),
         f
     )
-
-
